c80abc6815e795f30c991caf0a16e5b583595366
K3rhos <30273537+K3rhos@users.noreply.github.com>
2024-11-07 02:30:14 +0100

    Release v0.2

--- Source/Application/Application.cpp
+++ Source/Application/Application.cpp
@@ -14,34 +14,6 @@ void Application::Initialize(HMODULE _Module)
 		{
 			NoClip::Update();
 
-			/* EXPERIMENTAL
-			if (Input::IsKeyJustPressed(KEY_F4))
-			{
-				Actor localPlayerActor = GET_PLAYER_ACTOR(-1);
-
-				Vector3 position = GET_POSITION(localPlayerActor);
-				Vector3 rotation = Vector3();
-
-				ActorModel model = ACTOR_RIDEABLE_ANIMAL_Cow;
-
-				STREAMING_REQUEST_ACTOR(model, true, false);
-
-				while (!STREAMING_IS_ACTOR_LOADED(model, -1))
-				{
-					WAIT(0);
-				}
-
-				Layout layout = FIND_NAMED_LAYOUT("PlayerLayout");
-
-				Actor spawnedVehicle = CREATE_ACTOR_IN_LAYOUT(layout, "", model, &position, &rotation);
-
-				if (spawnedVehicle)
-				{
-					// RELEASE_ACTOR(spawnedVehicle);
-				}
-			}
-			*/
-
 			WAIT(0);
 		}
 	});
