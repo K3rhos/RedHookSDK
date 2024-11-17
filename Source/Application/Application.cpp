2df74e7ac9b15c165f571eb732c08848bb2fb263
K3rhos <30273537+K3rhos@users.noreply.github.com>
2024-11-17 17:59:43 +0100

    Updated Natives.h + No Clip code accordingly

--- Source/Application/Application.cpp
+++ Source/Application/Application.cpp
@@ -8,13 +8,13 @@ void Application::Initialize(HMODULE _Module)
 
 	ScriptRegister(_Module, []
 	{
-		_PRINT_HELP("<blue>No Clip Controls\n<purple>[F5] On/Off\n[WASD] Move\n[Ctrl] Go Down\n[Space] Go Up\n[A] Speed-\n[E] Speed+\n[Shift] Speed Boost", 15.0f, true, 1, 0, 0, 0, 0);
+		UNSORTED::PRINT_HELP_B("<blue>No Clip Controls\n<purple>[F5] On/Off\n[WASD] Move\n[Ctrl] Go Down\n[Space] Go Up\n[A] Speed-\n[E] Speed+\n[Shift] Speed Boost", 15.0f, true, 1, 0, 0, 0, 0);
 
 		while (true)
 		{
 			NoClip::Update();
 
-			WAIT(0);
+			ScriptWait(0);
 		}
 	});
 }
--- Source/Features/NoClip/NoClip.cpp
+++ Source/Features/NoClip/NoClip.cpp
@@ -5,7 +5,7 @@
 static Vector3 GET_FORWARD_VECTOR(Actor _Actor)
 {
 	Vector3 forward;
-	float heading = GET_HEADING(_Actor) + 90.0f;
+	float heading = UNSORTED::GET_HEADING(_Actor) + 90.0f;
 
 	heading *= Math<float>::DegToRad;
 
@@ -21,7 +21,7 @@ static Vector3 GET_FORWARD_VECTOR(Actor _Actor)
 static Vector3 GET_RIGHT_VECTOR(Actor _Actor)
 {
 	Vector3 right;
-	float heading = GET_HEADING(_Actor) + 180.0f;
+	float heading = UNSORTED::GET_HEADING(_Actor) + 180.0f;
 
 	heading *= Math<float>::DegToRad;
 
@@ -44,38 +44,38 @@ static void _PRINT_BOTTOM_FORMAT(const char* _Format, ...)
 	_vsnprintf_s(buffer, sizeof(buffer), _Format, vaBuffer);
 	va_end(vaBuffer);
 
-	_PRINT_BOTTOM(buffer, 0.5f, true, 0, 0, 0, 0);
+	UNSORTED::PRINT_SMALL_B(buffer, 0.5f, true, 0, 0, 0, 0);
 }
 
 
 
 void NoClip::Update()
 {
-	Actor localPlayerActor = GET_PLAYER_ACTOR(-1);
+	Actor localPlayerActor = UNSORTED::GET_PLAYER_ACTOR(-1);
 
-	if (Input::IsKeyJustPressed(KEY_F5) || (IS_BUTTON_DOWN(0, BUTTON_DPAD_DOWN) && IS_BUTTON_PRESSED(0, BUTTON_B)))
+	if (Input::IsKeyJustPressed(KEY_F5) || (UNSORTED::IS_BUTTON_DOWN(0, BUTTON_DPAD_DOWN) && UNSORTED::IS_BUTTON_PRESSED(0, BUTTON_B)))
 	{
 		s_NoClip ^= true;
 
-		SET_MOVER_FROZEN(localPlayerActor, s_NoClip);
+		UNSORTED::SET_MOVER_FROZEN(localPlayerActor, s_NoClip);
 
 		if (s_NoClip)
 		{
-			_PRINT_BOTTOM("No Clip <green>On", 0.5f, true, 0, 0, 0, 0);
+			UNSORTED::PRINT_SMALL_B("No Clip <green>On", 0.5f, true, 0, 0, 0, 0);
 		}
 		else
 		{
-			_PRINT_BOTTOM("No Clip <red>Off", 0.5f, true, 0, 0, 0, 0);
+			UNSORTED::PRINT_SMALL_B("No Clip <red>Off", 0.5f, true, 0, 0, 0, 0);
 		}
 	}
 
 	if (s_NoClip)
 	{
-		float leftStickX = GET_STICK_X(0, false, 0);
-		float leftStickY = GET_STICK_Y(0, false, 0);
+		float leftStickX = UNSORTED::GET_STICK_X(0, false, 0);
+		float leftStickY = UNSORTED::GET_STICK_Y(0, false, 0);
 
-		Vector3 position = GET_POSITION(localPlayerActor);
-		float cameraHeading = GET_OBJECT_HEADING(GET_GAME_CAMERA());
+		Vector3 position = UNSORTED::GET_POSITION(localPlayerActor);
+		float cameraHeading = UNSORTED::GET_OBJECT_HEADING(UNSORTED::GET_GAME_CAMERA());
 
 		const float stickDeadZone = 0.02f;
 
@@ -106,10 +106,10 @@ void NoClip::Update()
 		}
 
 		// Handle speed boost
-		if (Input::IsKeyPressed(KEY_LEFT_SHIFT) || IS_BUTTON_DOWN(0, BUTTON_STICK_LEFT))
+		if (Input::IsKeyPressed(KEY_LEFT_SHIFT) || UNSORTED::IS_BUTTON_DOWN(0, BUTTON_STICK_LEFT))
 			speed *= 2.0f;
 
-		float movementSpeed = TIMESTEP() * speed;
+		float movementSpeed = UNSORTED::TIMESTEP() * speed;
 
 		// Handle forward/backward/left/right movements
 		// NOTE: This support keyboard region, as an example is you're
@@ -134,16 +134,16 @@ void NoClip::Update()
 			position += GET_RIGHT_VECTOR(localPlayerActor) * leftStickX * movementSpeed;
 
 		// Handle going up/down
-		if (Input::IsKeyPressed(KEY_SPACE) || IS_BUTTON_DOWN(0, BUTTON_A))
+		if (Input::IsKeyPressed(KEY_SPACE) || UNSORTED::IS_BUTTON_DOWN(0, BUTTON_A))
 		{
 			position.y += movementSpeed;
 		}
-		else if (Input::IsKeyPressed(KEY_LEFT_CTRL) || IS_BUTTON_DOWN(0, BUTTON_X))
+		else if (Input::IsKeyPressed(KEY_LEFT_CTRL) || UNSORTED::IS_BUTTON_DOWN(0, BUTTON_X))
 		{
 			position.y -= movementSpeed;
 		}
 
-		TELEPORT_ACTOR(localPlayerActor, &position, false, false, false);
-		SET_ACTOR_HEADING(localPlayerActor, cameraHeading, false);
+		UNSORTED::TELEPORT_ACTOR(localPlayerActor, &position, false, false, false);
+		UNSORTED::SET_ACTOR_HEADING(localPlayerActor, cameraHeading, false);
 	}
 }
