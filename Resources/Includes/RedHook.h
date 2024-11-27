5c39d5d97ead746e213588e663250b491a23d132
K3rhos <30273537+K3rhos@users.noreply.github.com>
2024-11-28 00:31:08 +0100

    Fixed non updated SDK

--- Resources/Includes/RedHook.h
+++ Resources/Includes/RedHook.h
@@ -8,6 +8,10 @@
 
 
 
+// Get minor and major version of RedHook
+DLL_IMPORT int RH_GetMinorVersion();
+DLL_IMPORT int RH_GetMajorVersion();
+
 enum LogType
 {
 	Log_Info = 1,
@@ -22,7 +26,6 @@ DLL_IMPORT void KeyboardHandlerUnregister(f_KeyboardHandler _Function);
 
 DLL_IMPORT void ScriptWait(uint64_t _Ms);
 DLL_IMPORT void ScriptRegister(HMODULE _Module, void(*_Function)());
-DLL_IMPORT void ScriptRegisterAdditionalThread(HMODULE _Module, void(*_Function)());
 DLL_IMPORT void ScriptUnregister(void(*_Function)());
 DLL_IMPORT void ScriptUnregister(HMODULE _Module);
 
@@ -46,34 +49,9 @@ DLL_IMPORT void RH_DrawImage(uint64_t _TextureId, float _X, float _Y, float _Wid
 DLL_IMPORT void RH_DrawRect(float _X, float _Y, float _Width, float _Height, int _R, int _G, int _B, int _A);
 DLL_IMPORT void RH_DrawText(const char* _Text, float _X, float _Y, int _FontId, float _FontScale, int _R, int _G, int _B, int _A, TextAlignment _TextAlignment);
 DLL_IMPORT float RH_GetTextWidth(const char* _Text, int _FontId, float _FontScale);
-DLL_IMPORT void RH_GetTextHeight(const char* _Text, int _FontId, float _FontScale, void(*_Callback)(float));
+DLL_IMPORT void RH_GetTextHeight(const char* _Text, int _FontId, float _FontScale, float* _OutputValue);
 DLL_IMPORT void RH_SetTextWrapWidth(float _WrapWidth);
 
 
 
-enum Button : int
-{
-    BUTTON_NONE = -1,
-    BUTTON_TRIGGER_LEFT = 0,
-    BUTTON_TRIGGER_RIGHT = 1,
-    BUTTON_BUMPER_LEFT = 2,
-    BUTTON_BUMPER_RIGHT = 3,
-    BUTTON_Y = 4,
-    BUTTON_B = 5,
-    BUTTON_A = 6,
-    BUTTON_X = 7,
-    BUTTON_BACK = 8,
-    BUTTON_STICK_LEFT = 9,
-    BUTTON_STICK_RIGHT = 10,
-    BUTTON_START = 11,
-    BUTTON_DPAD_UP = 12,
-    BUTTON_DPAD_RIGHT = 13,
-    BUTTON_DPAD_DOWN = 14,
-    BUTTON_DPAD_LEFT = 15,
-
-    NUM_BUTTONS
-};
-
-DLL_IMPORT void RH_ButtonDisable(Button _Button, bool _Disabled);
-
-DLL_IMPORT bool IsZombieDLCActive();
+DLL_IMPORT void RH_ActionDisable(const char* _ActionName, bool _Disabled);
